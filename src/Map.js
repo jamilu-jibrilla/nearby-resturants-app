23132cdb1e60d5dbd16a6d4e410ae16606bd774c
jamil <jamilujibrilla01@gmail.com>
2022-03-08 01:48:15 +0100

    added marker to map

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,6 @@
 import {useState,useRef,useEffect } from "react";
 import '@tomtom-international/web-sdk-maps/dist/maps.css';
-import tt from '@tomtom-international/web-sdk-maps';
+import * as tt from '@tomtom-international/web-sdk-maps';
 
 const Map = () => {
 
@@ -8,61 +8,50 @@ const Map = () => {
     const [mapLatitude, setMapLatitude] = useState(9.2795);
     const [mapLongtitude, setMapLongtitude] = useState(12.4582);
     const [map, setMap] = useState({});
-    const [markerText, setMarkerText] = useState('Default Marker');
-    const [initialized, setInitialized] = useState(false);
-    const color = '#0A5B6J';
-    const language = 'en';
-    const style =  'basic_main';
-    const title =  'Default Map';
+    const [mapLoaded, setMapLoaded] = useState(false)
 
-  const addMarker = () => {
-    const targetCoordinates = [mapLongtitude, mapLatitude];
-
-    const marker = new tt.Marker({
-      color: color
-    })
-      .setLngLat(targetCoordinates)
-      .addTo(map);
-
-    var popupOffsets = {
-      top: [0, 0],
-      bottom: [0, -50],
-      left: [25, -35],
-      right: [-25, -35],
-    };
-
-    var popup = new tt.Popup({
-      offset: popupOffsets,
-    }).setHTML(markerText);
-
-    marker.setPopup(popup);
-    marker.togglePopup();
-  };
-  
-    useEffect(() => {
-        
+  function addMarker() {
+    //   if(mapLoaded) {
+    //     var marker = new tt.Marker({
+    //     color: ,
+    //   }).setLngLat[mapLongtitude, mapLatitude].addTo(map)
+    // }
+    var markerElement = document.createElement("div")
+    markerElement.className = "marker"
+    markerElement.style.backgroundColor = "#ABABAB"
+    markerElement.style.width = '10px'
+    markerElement.style.height = '10px'
+     var marker = new tt.Marker({element: markerElement, anchor: 'center',}).setLngLat([mapLongtitude, mapLatitude]).addTo(map)
+    return marker
+  }
+    useEffect(() => {  
         var map = tt.map({
             key: "qQMtZMYW4RAyf2frPAyIW1Az1jjBRAYC",
             container: mapElement.current,
             center: [mapLongtitude, mapLatitude],
-            style: { map:"basic_night"},
-            zoom: 14,
+            style: { map:"hybrid_main"},
+            color:"#0A5B6J",
+            zoom: 15,
+            title: "Hybrid map"
         })
         setMap(map);
-        setInitialized(true);
+        setMapLoaded(true)
         return () => map.remove();
-    })
-    useEffect(() => {
-        if (initialized) {
-          addMarker();
-        }
-    }, [initialized]);
+    },[])
     
+    useEffect(()=>{
+      if(mapLoaded) {
+        addMarker()
+      }
+    },[mapLoaded])
 
     
     return (
+      <div>
+        {/* <button style={{position: "absolute", bottom: "0em", left: "50%"}} onClick={addMarker}>click</button> */}
         <div style={{height: "100vh", width: "100vw"}} ref={mapElement} className="Map">
         </div>  
+      </div>
     )
 }
 export default Map
