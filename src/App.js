f951b7d69b87ef6716e584bfe297b09881e94626
jamil <jamilujibrilla01@gmail.com>
2022-03-12 15:22:20 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./Home";
 function App() {
   return (
     <div className="App">
-      <Router basename="/Home">
+      <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/Map" element={<Map />} />
