7a9648e42ba8797e06f28a332e832ee177dddfb3
jamil <jamilujibrilla01@gmail.com>
2022-03-12 18:02:11 +0100

    route update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  HashRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import ViewResturants from "./ViewResturants";
@@ -11,15 +6,13 @@ import Map from "./Map";
 import Home from "./Home";
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/Map" element={<Map />} />
-          <Route path="/ViewResturants" element={<ViewResturants />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Map" element={<Map />} />
+        <Route path="/ViewResturants" element={<ViewResturants />} />
+      </Routes>
+    </div>
   );
 }
 
