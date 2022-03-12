96fc5fa63231ff51bb2318cb80a041bb8f10e49c
jamil <jamilujibrilla01@gmail.com>
2022-03-12 10:37:59 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import './App.css';
-import ViewResturants from './ViewResturants';
-import Map from './Map';
-import Home from './Home';
+import "./App.css";
+import ViewResturants from "./ViewResturants";
+import Map from "./Map";
+import Home from "./Home";
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route path='/Map' element={<Map/>}/>
-          <Route path='/ViewResturants' element={<ViewResturants/>}/>
-      </Routes>
-    </Router>
+      <Router basename="/">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/Map" element={<Map />} />
+          <Route path="/ViewResturants" element={<ViewResturants />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
