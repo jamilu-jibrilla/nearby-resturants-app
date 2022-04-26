aca037988fc847c8d7181f145a4cf81df1c40063
jamil <jamilujibrilla01@gmail.com>
2022-04-26 07:20:58 +0100

    minor fix

--- src/ViewResturants.js
+++ src/ViewResturants.js
@@ -52,7 +52,7 @@ const ViewResturants = () => {
   return (
     <div className="ViewResturants">
       <div className="heading side-nav">
-        <h2 className="text-2xl">All resturants</h2>
+        <h2 className="text-2xl my-2">All resturants</h2>
 
         <a href="/Map">
           <button className="rounded py-1 px-3 md:py-1 my-5 text-black  flex items-center  md:px-5 bg-white ">
