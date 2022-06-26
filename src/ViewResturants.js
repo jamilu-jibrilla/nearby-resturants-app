bd153dbf0e61b4af8e77de1177edd6b93eecf015
jamil <jamilujibrilla01@gmail.com>
2022-06-26 02:33:51 +0100

    api updates

--- src/ViewResturants.js
+++ src/ViewResturants.js
@@ -50,11 +50,11 @@ const ViewResturants = ({appData}) => {
                 <img class="absolute inset-0 h-full w-full object-cover" src={appData.pictures[index].length < 1 ? "https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" : appData.pictures[index][Math.floor(Math.random() * appData.pictures[index].length)].prefix+"500x300"+appData.pictures[index][Math.floor(Math.random() * appData.pictures[index].length)].suffix} alt=""/>
               </div>
               <div class="p-4">
-                <span class="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">Open</span>
+                <span class="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">{place.hours.open_now ? "open now" : "closed now"}</span>
                 <h2 class="mt-2 mb-2  font-bold">{place.name}</h2>
-                <p class="text-sm">{place.description ?  place.description : 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper nulla non metus auctor fringilla'}.</p>
+                <p class="text-sm">{place.description ?  place.description : 'No description'}.</p>
                 <div class="mt-3 flex items-center">
-                  <span class="text-sm font-semibold">ab</span>&nbsp;<span class="font-bold text-xl">45,00</span>&nbsp;<span class="text-sm font-semibold">€</span>
+                  <span class="text-sm font-semibold"></span>&nbsp;<span class="font-bold text-xl"></span>&nbsp;<span class="text-sm font-semibold"></span>
                 </div>
               </div>
               <div class="p-4 border-t border-b text-xs text-gray-700">
