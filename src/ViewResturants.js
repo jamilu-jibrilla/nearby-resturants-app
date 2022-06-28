e4338c6535b9716aa1fe2ac28b3c01a029a1381b
jamil <jamilujibrilla01@gmail.com>
2022-06-28 22:30:33 +0100

    changes

--- src/ViewResturants.js
+++ src/ViewResturants.js
@@ -28,7 +28,7 @@ const ViewResturants = ({appData}) => {
     <div className="ViewResturants">
       <div className="heading flex items-center justify-center">
         <div className="relative flex items-center ">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-5 searchbtn absolute" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-6 searchbtn absolute" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
         <input type="text" placeholder="Search resturants..." className="relative"/>
