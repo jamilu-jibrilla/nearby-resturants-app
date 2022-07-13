34f8f0ed306fc2fbf3e6b72c61181ff69b94ebdb
jamil <jamilujibrilla01@gmail.com>
2022-07-13 08:00:57 +0100

    added search bar functionality

--- src/App.js
+++ src/App.js
@@ -1,69 +1,65 @@
 import {useState, useEffect} from "react"
-import { BrowserRouter as Router, Route, Routes,  } from "react-router-dom";
+import { Route, Routes,  } from "react-router-dom";
 import "./App.css";
 import ViewResturants from "./ViewResturants";
-import Map from "./Map";
 import Home from "./Home";
 
 function App() {
   
   const [appData, setAppData] =  useState({
-    pictures:"",
-    data:"",
-    lat:null,
-    long:null,
-  
+    pictures:[],
+    data: [],
+    lat: "",
+    long: "",
+    input:""
   })
 
-  useEffect(()=> {
-    // while(appData.lat===null && appData.long===null) {
-      // alert("please turn on your location")
-    // }
 
-    navigator.geolocation.getCurrentPosition((position) => {
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition( (position) => {
       setAppData({
         ...appData,
         lat: `${position.coords.latitude}`,
         long: `${position.coords.longitude}`
       })
     });
-    
+  },[])
 
-    
-    const options = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: 'fsq3O/V4qgl9Xwc7/J2XjC24IKwNOvbtI89PeWz/7LeD1+g='
-      }
-    };
 
-    if(appData.lat && appData.long) {
-      
+  useEffect(()=> {
       async function fetchData() {
+        const options = {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: 'fsq3O/V4qgl9Xwc7/J2XjC24IKwNOvbtI89PeWz/7LeD1+g='
+          }
+        };
+        
         let res = await fetch(`https://api.foursquare.com/v3/places/search?ll=${appData.lat}%2C${appData.long}&radius=100000&categories=13065&fields=timezone%2Ccategories%2Cfsq_id%2Cname%2Clocation%2Ctastes%2Cmenu%2Cprice%2Crating%2Chours%2Cdescription%2Cchains%2Cfeatures%2Csocial_media&limit=50`,options)
-        let data= await res.json()
-        Promise.all(data.results.map( place => {
-           return fetch(`https://api.foursquare.com/v3/places/${place.fsq_id}/photos?limit=40&sort=NEWEST&classifications=food%2Coutdoor%2Cmenu%2Cindoor`,options)
-          .then(res=> res.json())
-        })).then(data2 => setAppData({
+        let data = await res.json()
+        Promise.all(data.results.map( async place => {
+          const res = await fetch(`https://api.foursquare.com/v3/places/${place.fsq_id}/photos?limit=40&sort=NEWEST&classifications=food%2Coutdoor%2Cmenu%2Cindoor`, options);
+          return await res.json();
+        }))    
+        .then(images => setAppData({
           ...appData,
           data:data.results,
-          pictures:data2  
+          pictures: images  
         }))
       }
-      fetchData()
-    }
+    fetchData()
+
+  },[appData.long])
 
 
-  },[appData.lat && appData.long])
+
   
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />} />
-        {/* <Route path="/Map" element={<Map />} /> */}
-        <Route path="/ViewResturants"  element={<ViewResturants appData={appData}/>} />
+        <Route path="/ViewResturants"  element={<ViewResturants appData={appData} setAppData={setAppData}/>} />
       </Routes>
     </div>
   );
--- src/ViewResturants.js
+++ src/ViewResturants.js
@@ -1,10 +1,24 @@
-import { useState, useRef, useEffect } from "react";
-import axios from "axios";
 import "./ViewResturants.css";
-const ViewResturants = ({appData}) => {
-  console.log(appData)
-  console.log(appData.pictures[1])
+const ViewResturants = ({appData, setAppData}) => {
 
+
+  const filterByName = (e) => {
+    setAppData({
+      ...appData,
+      input:e.target.value
+    })
+  } 
+  
+  let images = []
+
+  let filteredData =  appData.data.filter((item, index) => {
+    if(item.name.toLowerCase().includes(appData.input.toLowerCase())) {
+       images.push(appData.pictures[index])
+       return item 
+    }
+  })
+  
+  
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -28,36 +42,41 @@ const ViewResturants = ({appData}) => {
     <div className="ViewResturants">
       <div className="heading flex items-center justify-center">
         <div className="relative flex items-center ">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-6 searchbtn absolute" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-6 searchbtn absolute" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
-        <input type="text" placeholder="Search resturants..." className="relative"/>
+        <input onChange={filterByName} type="text" placeholder="Search resturants..." className="relative"/>
         </div>
       </div>
       <div className="main flex justify-center flex-wrap">
-        {appData.data ? (
-          appData.data.map((place, index) => (          
-            <div class="card mx-7 my-4  c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-              <div class="relative pb-48 overflow-hidden">
-                <img class="absolute inset-0 h-full w-full object-cover" src={appData.pictures[index].length < 1 ? "https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" : appData.pictures[index][Math.floor(Math.random() * appData.pictures[index].length)].prefix+"500x300"+appData.pictures[index][Math.floor(Math.random() * appData.pictures[index].length)].suffix} alt=""/>
+        {filteredData.length > 0 ? (
+          filteredData.map((place, index) => (          
+            <div key={place.fsq_id} className="card mx-7 my-4  c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+              <div className="relative pb-48 overflow-hidden">
+                  <img className="absolute inset-0 h-full w-full object-cover" src={images[index].length < 1 ? "https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" : images[index][Math.floor(Math.random() * images[index].length)].prefix+"500x300"+images[index][Math.floor(Math.random() * images[index].length)].suffix} alt="restaurants pic"/>
               </div>
-              <div class="p-4">
-                <span class="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">{place.hours.open_now ? "open now" : "closed now"}</span>
-                <h2 class="mt-2 mb-2  font-bold">{place.name}</h2>
-                <p class="text-sm">{place.description ?  place.description : 'No description'}.</p>
-                <div class="mt-3 flex items-center">
-                  <span class="text-sm font-semibold"> {place.hours.display}</span>&nbsp;<span class="font-bold text-xl"></span>&nbsp;<span class="text-sm font-semibold"></span>
+              <div className="p-4">
+                <span className="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">{place.hours.open_now ? "open now" : "closed now"}</span>
+                <h2 className="mt-2 mb-2  font-bold">{place.name}</h2>
+                <p className="text-sm">{place.description ?  place.description : 'No description'}.</p>
+                <div className="mt-3 flex items-center">
+                  <span className="text-sm font-semibold"> {place.hours.display}</span>&nbsp;<span className="font-bold text-xl"></span>&nbsp;<span className="text-sm font-semibold"></span>
                 </div>
               </div>
-              <div class="p-4 border-t border-b text-xs text-gray-700">
-                <span class="flex items-center mb-1">
-                  <i class="far fa-clock fa-fw mr-2 text-gray-900"></i>{place.categories[0].name}
+              <div className="p-4 border-t border-b text-xs text-gray-700">
+                <span className="flex items-center mb-1">
+                  <i className="far fa-clock fa-fw mr-2 text-gray-900"></i>{place.categories[0].name}
                 </span> 
-                <span class="flex items-center">
-                  <i class="far fa-address-card fa-fw text-gray-900 mr-2"></i> {place.location.formatted_address}
+                <span className="flex items-center">
+                  <i className="far fa-address-card fa-fw text-gray-900 mr-2"></i> {place.location.formatted_address}
                 </span>        
               </div>
-              <div class="p-4 flex items-center text-sm text-gray-600"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-yellow-500"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-gray-400"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><span class="ml-2">{place.rating}</span></div>
+              <div className="p-4 flex items-center text-sm text-gray-600">
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4 fill-current ${place.rating/2 >= 1 ? "text-yellow-500":"text-gray-400"}`}><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg>
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4 fill-current ${place.rating/2 >= 2 ? "text-yellow-500":"text-gray-400"}`}><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg>
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4 fill-current ${place.rating/2 >= 3 ? "text-yellow-500":"text-gray-400"}`}><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg>
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4 fill-current ${place.rating/2 >= 4 ? "text-yellow-500":"text-gray-400"}`}><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg>
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4 fill-current ${place.rating/2 >= 5 ? "text-yellow-500":"text-gray-400"}`}><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg><span className="ml-2">{place.rating ? (place.rating/2).toFixed(1): "no rating"}</span></div>
           </div>
           
           ))
@@ -73,26 +92,27 @@ const ViewResturants = ({appData}) => {
           >
             <p> 
                 Loading 
-            <svg class=" animate-spin h-5 w-5 absolute inline-block ml-4 border s  ..." viewBox="0 0 24 24">
+            <svg className=" animate-spin h-5 w-5 absolute inline-block ml-4 border s  ..." viewBox="0 0 24 24">
             </svg> <br></br>
                 Ensure sure your location is on.
             </p>
           </div>
         )}
       </div>
+
       <button
         type="button"
         onClick={scrollToTop}
         data-mdb-ripple="true"
         data-mdb-ripple-color="light"
-        className="scroll  p-3 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out hidden bottom-5 right-5 fixed"
+        className="scroll  p-3 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out hidden bottom-8 right-7 fixed"
         id="btn-back-to-top"
       >
         <svg
           aria-hidden="true"
           focusable="false"
           data-prefix="fas"
-          class="w-4 h-4"
+          className="w-4 h-4"
           role="img"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 448 512"
