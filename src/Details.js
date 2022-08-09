59ce7ebe81d42eec8f47a8fe034f64dac7ce9a90
jamil <jamilujibrilla01@gmail.com>
2022-08-09 16:30:46 +0100

    added details page

--- src/Details.js
+++ src/Details.js
@@ -8,6 +8,8 @@ const {id} = useParams()
 const [mapLatitude, setMapLatitude] = useState(null);
 const [mapLongtitude, setMapLongtitude] = useState(null);
 const [photo, setPhoto] = useState([]);
+const [data, setData] = useState([]);
+const [res, setRes] = useState(null)
 
 useEffect(()=> {
     const options = {
@@ -23,46 +25,47 @@ useEffect(()=> {
         .then(response => response.json())
         .then(response => {
             setPhoto(response)
-            console.log(response)
+            setData(response)
         })
         .catch(err => alert("photo site: "+err));
-      
       fetch(`https://api.foursquare.com/v3/places/${id}?fields=timezone%2Ccategories%2Cfsq_id%2Cname%2Clocation%2Ctastes%2Cmenu%2Cprice%2Crating%2Chours%2Cdescription%2Cchains%2Cfeatures%2Csocial_media%2Cgeocodes`, options)
         .then(response => response.json())
         .then(response => {
             setMapLatitude(response.geocodes.main.latitude)
             setMapLongtitude(response.geocodes.main.longitude)
+            console.log(response)
+            setRes(response)
         })
         .catch(err => alert("data site: "+ err));
 },[])
 
     return (
     <div className='overflow-x-hidden'>
-        {(mapLatitude && mapLongtitude ) ? <> <Map mapLatitude={mapLatitude} mapLongtitude={mapLongtitude}/> 
-        <div className='detail flex justify-between w-screen  mt-8 '>
-            <section className=' w-1/3 px-4'>
-                <h1 className='text-2xl mb-5 font-medium'>Features</h1>
+        {(mapLatitude && mapLongtitude && data ) ? <> <Map mapLatitude={mapLatitude} mapLongtitude={mapLongtitude} res={res}/> 
+        <div className='detail flex flex-col md:flex-row w-screen  mt-8 '>
+            <section className='w-[100%] md:w-1/3 px-4'>
+                <h1 className='text-2xl mb-5 font-medium'><u> Features </u></h1>
                 <div>
                     <div className='mb-4'>
-                        <span> Accepts credit-card payment:</span>
-                        <span>True</span>
+                        <span> Accepts delivery:</span>
+                        <span>{res !== null ? JSON.stringify(res.features.services.delivery) : "not specified"}</span>
                     </div>
+
                     <div className='mb-4'>
-                        <span> Accepts credit-card payment:</span>
-                        <span>True</span>
-                    </div><div className='mb-4'>
-                        <span> Accepts credit-card payment:</span>
-                        <span>True</span>
-                    </div><div className='mb-4'>
-                        <span> Accepts credit-card payment:</span>
-                        <span>True</span>
+                        <span> Accepts credit card :</span>
+                        <span>{res !== null ? JSON.stringify(res.features.payment.credit_cards.accepts_credit_cards) : "not specified"}</span>
                     </div>
-                    
+
+                    <div className='mb-4'>
+                        <span>Allows Reservations:</span>
+                        <span>{res !== null ? JSON.stringify(res.features.services.dine_in.reservations) : "not specified"}</span>
+                    </div>
+
                 </div>
             </section>
-            <section className=' w-1/3 '>
-                <h1 className='text-2xl font-medium mb-5'>Photos</h1>
-                <div className='pics flex flex-wrap '>
+            <section className=' w-[100%] md:w-1/3'>
+                <h1 className='text-2xl font-medium mb-5'><u> Photos </u></h1>
+                <div className='pics flex flex-wrap p-3 md:p-0'>
                     
                     {photo.length > 0 ? photo.map(item => {
                     return <img className='mr-2 mb-2' src={item.prefix+"500x300"+item.suffix} alt='restaurant'  width="130px" height="150px"/>
@@ -73,12 +76,8 @@ useEffect(()=> {
                     <img className='mr-2' src='https://image.shutterstock.com/image-photo/group-happy-friends-having-breakfast-260nw-1201677928.jpg' alt='restaurant'  width="130px" height="150px"/>
                     <img className='mr-2' src='https://image.shutterstock.com/image-photo/group-happy-friends-having-breakfast-260nw-1201677928.jpg' alt='restaurant'  width="130px" height="150px"/>
                     <img className='mr-2' src='https://image.shutterstock.com/image-photo/group-happy-friends-having-breakfast-260nw-1201677928.jpg' alt='restaurant'  width="130px" height="150px"/>
-                    <img className='mr-2' src='https://image.shutterstock.com/image-photo/group-happy-friends-having-breakfast-260nw-1201677928.jpg' alt='restaurant'  width="130px" height="150px"/>
                     </>}
                 </div>
-            </section>
-            <section className=' w-1/3'>
-                <h1 className='text-2xl font-medium mb-5'>Social media</h1>
             </section>
         </div>
         </>
--- src/Map.js
+++ src/Map.js
@@ -2,10 +2,14 @@ import { useState, useRef, useEffect } from "react";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import tt from "@tomtom-international/web-sdk-maps";
 
-const Map = ({mapLatitude, mapLongtitude  }) => {
+import * as ttmaps from "@tomtom-international/web-sdk-maps"
+import * as ttS from "@tomtom-international/web-sdk-services";
+
+const Map = ({mapLatitude, mapLongtitude, res }) => {
   const mapElement = useRef();
   const [map, setMap] = useState({});
   const [mapLoaded, setMapLoaded] = useState(false);
+  const [result, setResult] = useState({});
 
   const addMarker = (l1, l2, markerText) => {
     let markerElement = document.createElement("div");
@@ -33,10 +37,48 @@ const Map = ({mapLatitude, mapLongtitude  }) => {
   };
 
   useEffect(() => {
-    if (mapLoaded) {;
+    if (mapLoaded) {
+      const calculateRoute = (startLatitude, startLongitude, destinationLatitude, destinationLongitude) => {
+        ttS.services
+          .calculateRoute({
+            key: "qQMtZMYW4RAyf2frPAyIW1Az1jjBRAYC",
+            locations: `${startLatitude},${startLongitude}:${destinationLatitude},${destinationLongitude}`
+          })
+          // .go()
+          .then(function (routeData) {
+            map.setCenter([ parseFloat(startLongitude), parseFloat(startLatitude)]);
+            map.setZoom(13)
+            console.log(routeData.toGeoJson());
+            const data = routeData.toGeoJson();
+            setResult(data);
+            // map.addLayer({
+            //   'id': 'route',
+            //   'type': 'line',
+            //   'source': {
+            //       'type': 'geojson',
+            //       'data': data
+            //   },
+            //   'paint': {
+            //       'line-color': '#4a90e2',
+            //       'line-width': 6
+            //   }
+            // });
+            // var bounds = new tt.LngLatBounds();
+            // data.features[0].geometry.coordinates.forEach(function(point) {
+            //     bounds.extend(tt.LngLat.convert(point));
+            // });
+            // map.fitBounds(bounds, { duration: 0, padding: 6 });
+            
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+
       addMarker(mapLongtitude, mapLatitude, "Destination")
       const showPosition = (position) => {
         addMarker(position.coords.longitude, position.coords.latitude, "My location")
+        calculateRoute(position.coords.latitude, position.coords.longitude, mapLatitude, mapLongtitude)
       }
       navigator.geolocation.getCurrentPosition(showPosition)
     }
@@ -44,10 +86,14 @@ const Map = ({mapLatitude, mapLongtitude  }) => {
 
   useEffect(() => {
     
-    let map = tt.map({
+    let map = ttmaps.map({
       key: "qQMtZMYW4RAyf2frPAyIW1Az1jjBRAYC",
       container: mapElement.current,
       center: [mapLongtitude, mapLatitude],
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true
+      },
       style: { map: "basic_main" },
       color: "#0A5B6J",
       zoom: 13,
@@ -64,12 +110,39 @@ const Map = ({mapLatitude, mapLongtitude  }) => {
     return () => map.remove();
   }, []);
 
+  const resultList = result.features ? (
+    <div className="" key={result.id}>
+      <div className="m-5">
+      <h4 className='text-3xl mb-2 font-bold'> About </h4>
+          <h4 className="">
+            Name: {res.name}
+          </h4>
+          <h4>
+            Address : {res.location.address}
+          </h4>
+        <div className="">
+          <h4>
+            Distance in KM : {result.features[0].properties.summary.lengthInMeters / 1000}
+          </h4>
+          <h4>
+            Time Estimate for Journey is
+            {` ${(result.features[0].properties.summary.travelTimeInSeconds / 60).toFixed(1)} minutes`}
+          </h4>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <h4>Add location to get route details</h4>
+  );
   return (
+    <>
       <div
         style={{ height: "40vh", width: "100vw" }}
         ref={mapElement}
         className="Map"
       ></div>
+      {resultList}
+    </>
   );
 };
 export default Map;
